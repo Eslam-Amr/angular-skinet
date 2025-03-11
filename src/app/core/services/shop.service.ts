506b79072da53ae05c9f7ba2ec82399c9b56a0c4
https://github.com/Eslam-Amr/portfolio <eslamamr537@gmail.com>
2025-03-11 02:53:42 +0200

    end of section 10

--- src/app/core/services/shop.service.ts
+++ src/app/core/services/shop.service.ts
@@ -43,6 +43,11 @@ getProducts(shopParams : ShopParams){
 //   // return this.http.get<Pagination<Product>>(this.baseUrl+'products')
 
 // }
+getProduct(id :number)
+{
+  return this.http.get<Product>(this.baseUrl+'products/'+id)
+}
+
 getBrands(){
   if(this.brands.length>0)return ;
   return this.http.get<string[]>(this.baseUrl+'products/brands').subscribe({
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [
-    MatIcon,MatBadge,MatButton
+    MatIcon,MatBadge,MatButton,RouterLink,RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
